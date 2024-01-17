b2478ff8ecb41418b63edc7fd0cefedd0220da5e
Asgar-Asgarov <asgar.y.asgarov@gmail.com>
2024-01-17 05:44:15 +0400

    feature:Header component finished

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,7 +3,11 @@ import './Header.css'
 export const Header = () => {
   return (
     <div className='header'>
-      Header
+      <div className="headerTitles">
+        <span className="headerTitleSm">React & node</span>
+        <span className="headerTitleLg">Blog</span>
+      </div>
+      <img src="https://images.unsplash.com/photo-1595104105148-ff8cfa0b42b9?q=80&w=1631&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" className="headerImg" />
     </div>
   )
 }
