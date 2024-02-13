f4b22ffa0c19fcac3f7aac77e13561bacc0dcfae
Asgar-Asgarov <asgar.y.asgarov@gmail.com>
2024-02-13 23:50:54 +0400

    feature:sidebar about me and categories added

--- src/Components/sidebar/Sidebar.jsx
+++ src/Components/sidebar/Sidebar.jsx
@@ -2,8 +2,23 @@ import "./sidebar.css"
 
 const Sidebar = () => {
   return (
-    <div>
-      Sidebar
+    <div className="sidebar">
+      <div className="sidebarItem">
+        <span className="sidebarTitle">ABOUT ME</span>
+        <img src="" alt="" />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam illo voluptas odit excepturi, maxime reiciendis harum necessitatibus dolor, nihil non reprehenderit eos cum vitae? Soluta perspiciatis minus quos atque minima?</p>
+      </div>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">CATEGORIES</span>
+        <ul className="sidebarList">
+          <li className="sidebarListItem">Life</li>
+          <li className="sidebarListItem">Sport</li>
+          <li className="sidebarListItem">Music</li>
+          <li className="sidebarListItem">Style</li>
+          <li className="sidebarListItem">Tech</li>
+          <li className="sidebarListItem">Cinema</li>
+        </ul>
+      </div>
     </div>
   )
 }
